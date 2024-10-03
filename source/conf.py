48b938c86fbb5e168b8c8976d62fcfd12bc887fb
tangtang <a5511572@gmail.com>
2024-10-03 12:04:41 +0800

    Original Note of Python

---
+++ source/conf.py
@@ -0,0 +1,52 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = ''
+copyright = '2024, tangtang'
+author = 'tangtang'
+
+# -- General configuration ---------------------------------------------------
+# release = '0.1'
+# version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'myst_nb',
+]
+nb_execution_mode = 'off'
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
+html_favicon = 'Figure/JunpyIcon2.png'
+html_logo = 'Figure/JunpyIcon.png'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
---
+++ source/index.rst
@@ -0,0 +1,41 @@
+.. JunPy Website documentation master file, created by
+   sphinx-quickstart on Wed Jul 24 14:04:00 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Python Note
+=============
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Data Type
+
+   /Notebook/1_Tutorial_DataType.ipynb
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Useful Statement
+
+   /Notebook/2_Tutorial_Statement.ipynb
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Loop
+
+   /Notebook/3_Tutorial_Loop.ipynb
+
+.. toctree::
+   :maxdepth: 2
+   :caption: File Operator
+
+   /Notebook/4_Tutorial_FileLR.ipynb
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Ploting Figure
+
+   /Notebook/5_Tutorial_Ploting.ipynb
